382f95b123c18a06cac09578edcff96e16a26eba
Soumya Jindal <56805064+jindalsoumya@users.noreply.github.com>
2021-08-02 09:20:27 -0700

    Map okay need coord

--- src/components/Home/MapComponent.js
+++ src/components/Home/MapComponent.js
@@ -1,13 +1,25 @@
 import Container from '@material-ui/core/Container'
 import { Component } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+ import RoutineMachine from './RoutineMachine';
 import './MapStyling.css'
-import map from './map.png';
+import 'leaflet/dist/leaflet.css';
 
 class Map extends Component {
   render() {
     return (
         <Container className = "map-container">
-          <img src={map} className = "map-image"/>
+          <MapContainer
+             doubleClickZoom={false}
+             id="mapId"
+             zoom={14}
+             center={[33.5024, 36.2988]}
+             >
+                 <TileLayer
+                     url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+                 />
+                 <RoutineMachine/>
+            </MapContainer>
         </Container>
     );
   }
---
+++ src/components/Home/RoutineMachine.js
@@ -0,0 +1,34 @@
+import L from "leaflet";
+import { createControlComponent } from "@react-leaflet/core";
+import "leaflet-routing-machine";
+import data from './data.json'
+const createRoutineMachineLayer = (props) => {
+  // {data.geometry.coordinatesmap((item, index, element) => {
+  //   if(index < (data.segment_stats.data.length-1))
+  //   {
+   //     L.latLng(item[1], item[0])
+   //   }
+   // })}
+
+   const instance = L.Routing.control({
+    waypoints: [
+      L.latLng(33.52001088075479, 36.26829385757446),
+      L.latLng(33.50546582848033, 37.29547681726967),
+      L.latLng(33.50546582848033, 45.29547681726967)
+    ],
+    lineOptions: {
+      styles: [{ color: "#6FA1EC", weight: 4 }]
+    },
+    show: false,
+    addWaypoints: false,
+    routeWhileDragging: false,
+    draggableWaypoints: false,
+    fitSelectedRoutes: true,
+    showAlternatives: false
+  });
+
+  return instance;
+};
+
+const RoutingMachine = createControlComponent(createRoutineMachineLayer);
+export default RoutingMachine;
